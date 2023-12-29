75f338ead74167f0c8aed06b5d1f48c495615f35
brokensilver75 <rsmadhan75@gmail.com>
2023-12-29 15:14:06 +0530

    bullet and asteroids spawn

--- SpaceShooter/assets/Scripts/AsteroidSpawner.ts
+++ SpaceShooter/assets/Scripts/AsteroidSpawner.ts
@@ -1,10 +1,9 @@
-import { _decorator, CCInteger, Component, Node } from 'cc';
+import { _decorator, CCInteger, Component, instantiate, macro, Node, Prefab } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('AsteroidSpawner')
 export class AsteroidSpawner extends Component {
 
-    direction: number = 1;
     @property(
         {
             type: CCInteger,
@@ -13,23 +12,36 @@ export class AsteroidSpawner extends Component {
     )
     private asteroidSpawnerSpeed: number = 60;
 
+    @property(
+        {
+            type: Prefab,
+            tooltip: 'Asteroid'
+        }
+    )
+    private asteroid: Prefab;
+
+    onLoad() {
+        this.schedule(this.spawnAsteroid, 3, macro.REPEAT_FOREVER, 0);
+    }
+
     start() {
 
     }
 
     update(deltaTime: number) {
         
-        this.node.setPosition(this.node.getPosition().x, this.node.getPosition().y);
-        if (this.node.getPosition().x <= -221)
-        {
-
-        }
-
-        if (this.node.getPosition().x >= 221)
-        {
-
-        }
+        this.node.setPosition(this.node.getPosition().x + this.asteroidSpawnerSpeed * deltaTime, this.node.getPosition().y);
         
+        if (this.node.getPosition().x <= -221 || this.node.getPosition().x >= 221)
+        {
+            this.asteroidSpawnerSpeed *= -1;
+        }
+    }
+
+    spawnAsteroid() {
+        var enemy = instantiate(this.asteroid);
+        enemy.setWorldPosition(this.node.getPosition());
+        this.node.parent.addChild(enemy); 
     }
 }
 
